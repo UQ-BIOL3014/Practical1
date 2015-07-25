b1e50b14bfc87d01dcd3db658f933dcd83de5630
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-07-25 18:33:39 +1000

    Boilerplate code

---
+++ src/hydrophobic_detection.py
@@ -0,0 +1,12 @@
+# Given a sequence where each character is in a list,  this program reports
+# the hydrophobic residues
+
+hydrophobics = "FLIMVPAWG"
+
+sequence = ["M", "H", "K", "L"]
+
+for aa in sequence:
+    print "The amino acids is ", aa
+    if aa in hydrophobics:
+        print " It is hydrophobic"
+print "End of the program"
---
+++ src/one_to_three_letter_code.py
@@ -0,0 +1,10 @@
+"""
+This program converts a single letter amino acid symbol to the corresponding
+three letter code
+"""
+
+aa = "W"
+aacodes = {'Y': 'TYR', 'F': 'PHE', 'C': 'CYS', 'V': 'VAL', 'H': 'HIS',
+           'L': 'LEU', 'D': 'ASP', 'A': 'ALA', 'S': 'SER', 'E': 'GLU',
+           'W': 'TRP', 'P': 'PRO', 'I': 'ILE', 'R': 'ARG', 'K': 'LYS',
+           'N': 'ASN', 'M': 'MET', 'T': 'THR', 'G': 'GLY', 'Q': 'GLN'}
